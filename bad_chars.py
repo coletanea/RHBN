5a328e806d38a369adadecbc9424339013b920f2
Julia <jmoraeslia@gmail.com>
2023-11-11 12:23:19 -0300

    removing bad characters//

--- bad_chars.py
+++ bad_chars.py
@@ -1,13 +1,15 @@
-from unidecode import unidecode
-
-
 import os
 from unidecode import unidecode
 
 def sanitize_string(input_str):
-    return unidecode(input_str).lower().replace(' ', '_').replace("'", "_").replace('"', "_").replace('.', "_")
+    result = unidecode(input_str).lower()
+    result = result.replace(' ', '_')
+    result = result.replace("'", "_").replace('"', "_")
+    result = result.replace('.', "_")
+    result = result.replace(',', '')
+    result = result.replace('-', '_')
+    return result
 
-# Main directory where the files are located
 main_directory = './pages'
 
 def rename_files(directory):
@@ -15,31 +17,16 @@ def rename_files(directory):
         for file in files:
             current_path = os.path.join(folder, file)
             file_name, file_extension = os.path.splitext(file)
-
-            # Create a new name without special characters
             new_name = sanitize_string(file_name)
-
-            # Add the extension back
             new_name += file_extension
-
-            # Create the new file path
             new_path = os.path.join(folder, new_name)
-
-            # Rename the file
             os.rename(current_path, new_path)
 
-    # Rename folders
     for folder, subfolders, files in os.walk(directory):
         for subfolder in subfolders:
             current_path = os.path.join(folder, subfolder)
-
-            # Create a new folder name without special characters
             new_name = sanitize_string(subfolder)
-
-            # Create the new folder path
             new_path = os.path.join(folder, new_name)
-
-            # Rename the folder
             os.rename(current_path, new_path)
 
 rename_files(main_directory)
